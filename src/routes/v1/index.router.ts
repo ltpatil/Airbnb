c0ffa00c36e8792324b8327639fd0b8971358ca5
ltpatil <ltpatil.3088@gmail.com>
2026-05-03 14:35:35 +0530

    removed read file jargon

--- src/routes/v1/index.router.ts
+++ src/routes/v1/index.router.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import pingRouter from './ping.routes';
-import readFileRouter from './readFile.routes';
+
 import hotelRouter from './hotel.router';
 
 const v1Router = express.Router();
 
 v1Router.use('/ping', pingRouter);
-v1Router.use('/read',readFileRouter);
 
 v1Router.use('/hotels', hotelRouter);
 
--- src/routes/v1/readFile.routes.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { readFileHandler } from '../../controllers/readFile.controller';
-
-const readFileRouter = express.Router();
-
-readFileRouter.get('/',readFileHandler);
-export default readFileRouter;
