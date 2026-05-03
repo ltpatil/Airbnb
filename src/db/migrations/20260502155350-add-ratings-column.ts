16be189648c1eb14160a22e909167bdaf3cc018b
ltpatil <ltpatil.3088@gmail.com>
2026-05-03 11:15:04 +0530

    Add ratings and rating_count columns to hotels table migration

---
+++ src/db/migrations/20260502155350-add-ratings-column.ts
@@ -0,0 +1,22 @@
+import { QueryInterface } from "sequelize";
+export default {
+  async up (queryInterface : QueryInterface) {
+    await queryInterface.sequelize.query(
+      `
+        ALTER TABLE hotels 
+        ADD COLUMN ratings DECIMAL(3,2) DEFAULT NULL,
+        ADD COLUMN rating_count INT DEFAULT NULL;
+      `
+    )
+  },
+
+  async down (queryInterface : QueryInterface) {
+    await queryInterface.sequelize.query(
+      `
+        ALTER TABLE hotels 
+        DROP COLUMN ratings ,
+        DROP COLUMN rating_count;
+      `
+    )
+  }
+};
